d77491196a5f6115079dc64aaa5141faf866b50d
Nick Solomon <nsolomon@council.nyc.gov>
2018-12-17 11:33:35 -0500

    make better popups

--- code/farmers_boxes_map.R
+++ code/farmers_boxes_map.R
@@ -1,12 +1,13 @@
 library(leaflet)
 library(tidyverse)
 library(councildown)
+library(lubridate)
 
 source("code/load_farmers_boxes.R")
 
 pal <- colorFactor(nycc_pal("cool")(2), domain = markets_boxes$service_type)
 
-markets_boxes %>% 
+to_map <- markets_boxes %>% 
   # as_tibble() %>% 
   mutate_at(vars(sunday, monday, tuesday, wednesday, thursday, friday, saturday), as.character) %>% 
   mutate(hours = pmap(list(sunday, monday, tuesday, wednesday, thursday, friday, saturday), 
@@ -18,10 +19,29 @@ markets_boxes %>%
                               Friday = ..6, 
                               Saturday = ..7) %>% 
                        gather("Day", "Hours") %>% 
-                       drop_na(Hours))) %>%
-  mutate(caption = pmap_chr(list(facilityname, hours), ~paste(..1, "<hr>", knitr::kable(..2, format = "html")))) %>% 
+                       drop_na(Hours)),
+         start = paste(month.name[month(startdate)], day(startdate)),
+         end = paste(month.name[month(enddate)], day(enddate)),
+         start = ifelse(open_year_round, "January 1", start),
+         end = ifelse(open_year_round, "December 31", end)) %>% 
+  mutate(caption = pmap_chr(list(facilityname, 
+                                 hours, 
+                                 website, 
+                                 start,
+                                 end,
+                                 address,
+                                 accepts_ebt), ~paste("<a href='", ..3, "' target='_blank'>", ..1, "</a>", "<br>",
+                                                  "<small><em>", ..4, "-",  ..5, "<br>", ..6, "</small></em>",
+                                                  "<hr>", 
+                                                  knitr::kable(..2, format = "html"), "<br>",
+                                                  "EBT:", ifelse(..7, "Yes", "No")))) 
+market_map <- to_map %>%     
   leaflet() %>% 
   addProviderTiles("CartoDB.Positron") %>% 
   addCircles(color = ~pal(service_type), radius = 1,
              popup = ~caption) %>% 
-  addLegend(pal = pal, values = ~ service_type)
+  addLegend(pal = pal, values = ~ service_type,
+            title = "", position = "bottomleft")
+
+htmlwidgets::saveWidget(market_map, "market_map.html")
+file.rename("market_map.html", "results/market_map.html")
