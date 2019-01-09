feaa5a6737d9736fb9f043ecbf5aaa8b7bc481d2
Nick Solomon <nsolomon@council.nyc.gov>
2019-01-09 12:45:58 -0500

    Tweak styling

--- code/farmers_boxes_map.R
+++ code/farmers_boxes_map.R
@@ -17,19 +17,27 @@ make_caption <- function(facilityname,
                          accepts_ebt) {
   
   if (!is.na(website)){
-    out <- paste("<div style=\"font-family:'Open Sans', sans-serif;\">","<a href='", website, "' target='_blank'>", facilityname, "</a>", "<br>",
-                 "<small><em>", start, "-",  end, "<br>", address, "</small></em>",
-                 "<hr>", 
-                 knitr::kable(hours, format = "html"), "<br>",
-                 "EBT:", ifelse(accepts_ebt, "Yes", "No"),
-                 "</div>")
+    out <- paste(
+      #"<div style=\"font-family:'Open Sans', sans-serif;\">",
+      "<h4><a href='", website, "' target='_blank'>", facilityname, "</a></h4>",
+      "<small><em>", start, "-",  end, "<br>", address, "</em></small>",
+      "<hr>", 
+      knitr::kable(hours, format = "html"), "<br>",
+      "EBT:", ifelse(accepts_ebt, "Yes", "No")
+      #,
+      #"</div>"
+    )
   } else {
-    out <- paste("<div style=\"font-family:'Open Sans', sans-serif;\">", facilityname, "<br>",
-                 "<small><em>", start, "-",  end, "<br>", address, "</small></em>",
-                 "<hr>", 
-                 knitr::kable(hours, format = "html"), "<br>",
-                 "EBT:", ifelse(accepts_ebt, "Yes", "No"),
-                 "</div>")
+    out <- paste(
+      #"<div style=\"font-family:'Open Sans', sans-serif;\">",
+      "<h4>", facilityname, "</h4>",
+      "<small><em>", start, "-",  end, "<br>", address, "</em></small>",
+      "<hr>", 
+      knitr::kable(hours, format = "html"), "<br>",
+      "EBT:", ifelse(accepts_ebt, "Yes", "No")
+      #,
+      # "</div>"
+    )
   }
   
   return(out)
@@ -40,15 +48,15 @@ to_map <- markets_boxes %>%
   # as_tibble() %>% 
   mutate_at(vars(sunday, monday, tuesday, wednesday, thursday, friday, saturday), as.character) %>% 
   mutate(hours = pmap(list(sunday, monday, tuesday, wednesday, thursday, friday, saturday), 
-                     ~ tibble(Sunday = ..1,
-                              Monday = ..2, 
-                              Tuesday = ..3, 
-                              Wednesday = ..4, 
-                              Thursday = ..5, 
-                              Friday = ..6, 
-                              Saturday = ..7) %>% 
-                       gather("Day", "Hours") %>% 
-                       drop_na(Hours)),
+                      ~ tibble(Sunday = ..1,
+                               Monday = ..2, 
+                               Tuesday = ..3, 
+                               Wednesday = ..4, 
+                               Thursday = ..5, 
+                               Friday = ..6, 
+                               Saturday = ..7) %>% 
+                        gather("Day", "Hours") %>% 
+                        drop_na(Hours)),
          start = paste(month.name[month(startdate)], day(startdate)),
          end = paste(month.name[month(enddate)], day(enddate)),
          start = ifelse(open_year_round, "January 1", start),
@@ -62,17 +70,23 @@ to_map <- markets_boxes %>%
                                  accepts_ebt), make_caption)) 
 
 
+bounds <- st_bbox(to_map)
+names(bounds) <- NULL
+
 (market_map <- to_map %>% 
-  leaflet() %>% 
-  addCouncilStyle() %>% 
-  addPolygons(data = dists, fill = FALSE, weight = .5, color = "black", opacity = .2) %>% 
-  addCircles(color = ~pal(service_type), radius = 1.5,
-             popup = ~caption,
-             group = "markets") %>%
-  addLegend(pal = pal, values = ~ service_type,
-            title = "", position = "bottomleft") %>%
+    leaflet() %>% 
+    addCouncilStyle() %>% 
+    addCircleMarkers(color = ~pal(service_type), radius = 2,
+               popup = ~councilPopup(caption),
+               group = "markets",
+               fillOpacity = 1,
+               weight = 50,
+               opacity = 0) %>%
+    addLegend(pal = pal, values = ~ service_type,
+              title = "", position = "bottomleft") %>%
     addSearchOSM(options = list(position = "topright", collapsed = FALSE, zoom = 14, marker = TRUE)) %>% 
-    addControlGPS(options = gpsOptions(autoCenter = TRUE, setView = TRUE, maxZoom = 14))
+    addControlGPS(options = gpsOptions(autoCenter = TRUE, setView = TRUE, maxZoom = 14)) %>% 
+    setView(mean(bounds[c(1,3)]), mean(bounds[c(2,4)]), zoom = 10.5)
 )
 
 htmlwidgets::saveWidget(market_map, "market_map.html")
